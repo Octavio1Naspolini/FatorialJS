13562adef3fd298d02161a3b82a8e5022568982f
Octavio Naspolini <octavionaspolinineto@gmail.com>
2023-10-09 19:17:18 -0300

    Delete escola directory

--- escola/src/main/java/com/br/escola/Repository/AulaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.escola.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.escola.Model.Aula;
-
-public interface AulaRepository extends JpaRepository<Aula, Integer> {
-
-}
--- escola/src/main/java/com/br/escola/Repository/EstudanteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.escola.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.escola.Model.Estudante;
-
-public interface EstudanteRepository extends JpaRepository<Estudante, Integer> {
-
-}
